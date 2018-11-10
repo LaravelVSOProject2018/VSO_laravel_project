c62d3d1bce1e91a15eb0e8b05cb71b0aaa5c13c0
Borislav <33787838+bobooooooo@users.noreply.github.com>
2018-11-10 11:20:31 +0200

    database
    repair

--- app/Session.php
+++ app/Session.php
@@ -7,11 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Session extends Model
 {
     protected $fillable = [
-<<<<<<< HEAD
     	'name','start_datetime','duration','exam_id'];
-=======
-    	'kind','start_datetime','end_datetime','exam_id'];
->>>>>>> b87495bb90a8fafbe4840818e379f2229f64723a
 
     public function exam(){
     	return $this->belongsTo('App\Exam');
